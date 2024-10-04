6aae4432c833a31bda756c49d66c576ededf99a6
Avx <93741404+Golf1ABF@users.noreply.github.com>
2024-10-04 13:46:52 +0200

    Add project files.

---
+++ SoftDrinksGUI/Drinks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftDrinksGUI
+{
+    internal class Drinks
+    {
+        public string Nev { get; set; }
+        public string EdesitoAnyag { get; set; }
+        public int Ar { get; set; }
+        public string CsomagolasTipusa { get; set; }
+        public int GyumolcsTartalom { get; set; }
+        public int CsomagolasEgysege { get; set; }
+
+        public Drinks(string sor)
+        {
+            var s = sor.Split(";");
+            Nev = s[0];
+            EdesitoAnyag = s[1];
+            Ar = int.Parse(s[2]);
+            CsomagolasTipusa = s[3];
+            GyumolcsTartalom = int.Parse(s[4]);
+            CsomagolasEgysege = int.Parse(s[5]);
+        }
+    }
+}
---
+++ SoftDrinksGUI/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoftDrinksGUI
+{
+
+    public partial class MainWindow : Window
+    {
+        List<Drinks> lista = new List<Drinks>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            var rnd = new Random();
+            var sr = new StreamReader("../../../source/softDrinks.txt", System.Text.Encoding.UTF8);
+            sr.ReadLine();
+            while (!sr.EndOfStream) 
+            { 
+                lista.Add(new Drinks(sr.ReadLine()));
+            }
+            sr.Close();
+            //4. Feladat
+            maiAjanlat.Content = $"Ajánlatunk:{lista[rnd.Next(lista.Count)].Nev}";
+
+            //5. Feladat
+            var legolcsobbUdito = lista.MinBy(x => x.Ar);
+            legolcsobb.Content = $"{legolcsobbUdito.Nev} - {legolcsobbUdito.Ar} Ft";
+
+            //6. Feladat
+            hanyGyarto.Content = $"{lista.GroupBy(x => x.Nev.Split(" ")[0]).Count()} féle gyártó termékei közül választhatnak!";
+
+            //7. Feladat
+            var sw = new StreamWriter("../../../source/all.txt", false);
+            var uditoNevek = lista.GroupBy(x => x.Nev);
+            foreach (var item in uditoNevek)
+            {
+                sw.WriteLine($"{item.Key} {Math.Round(item.Average(x => x.GyumolcsTartalom), 2)}");
+            }
+            sw.Close();
+
+            //8. Feladat
+            sw = new StreamWriter("../../../source/sweetening.txt", false);
+            var edesitoSzerint = lista.GroupBy(x => x.EdesitoAnyag);
+            foreach (var item in edesitoSzerint)
+            {
+                sw.WriteLine($"{item.Key}-{item.Count()} db");
+            }
+            sw.Close();
+
+            sw = new StreamWriter("../../../source/offer.txt", false);
+            var felhasznaloAltalValasztott = lista.Where(x => x.Nev == gyartoKereses.Text);
+        }
+    }
+}
